427f4ae32df1660701de4458d28d85939a1a52c0
surya <kommojusurya61@gmail.com>
2026-04-07 20:20:55 +0530

    updated redirect uri

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -8,7 +8,7 @@ export default function LoginButton() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
   };
